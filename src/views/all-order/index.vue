ad7e492ca412b833f74aba2e049e03bdfe754b5f
505402896 <505402896@qq.com>
2021-04-18 23:04:52 +0800

    U 全部评论

--- src/views/all-order/index.vue
+++ src/views/all-order/index.vue
@@ -1,24 +1,117 @@
 <!--  -->
 <template>
-  <div>
+  <div class="container">
+    <m-header />
+    <el-table
+      ref="singleTable"
+      :data="tableData"
+      highlight-current-row
+      @current-change="handleCurrentChange"
+      style="width: 100%;margin:0 auto;">
+      <el-table-column type="index" label="序号"/>
+      <el-table-column prop="bid" label="订单号" />
+      <el-table-column prop="htype" label="房间类型" />
+      <el-table-column prop="pname" label="用户名" />
+      <el-table-column prop="inDay" label="入住日期" />
+      <el-table-column prop="outDay" label="退房日期" />
+      <el-table-column prop="status" key="status" label="状态">
+        <template slot-scope="scope">
+          <el-tag type="danger">{{ scope.row.status }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" min-width="200">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-view" size="mini" @click="showDetail(scope.row)">详细</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="del(scope.row)">删除</el-button>
+        </template> 
+      </el-table-column>
+    </el-table>
+    <m-detail :visible.sync="detailVisible" :detailRow="detailRow"/>
+    <pagination :total="total" :current-page.sync="currentPage"/>
   </div>
 </template>
 
 <script>
+import mHeader from './module/header'
+import mDetail from './module/detail'
+import Pagination from '@/components/common/Pagination'
 export default {
   name:'all-order',
   data () {
     return {
+      detailVisible: false,
+      detailRow: undefined,
+      tableData:[
+        {
+          bid:'2',
+          htype:'大床房',
+          pname:'王子杰',
+          inDay:'2020-1-2',
+          outDay:'2021-2-3',
+          status: '已入住'
+        },
+        {
+          bid:'2',
+          htype:'大床房',
+          pname:'王子杰',
+          inDay:'2020-1-2',
+          outDay:'2021-2-3',
+          status: '已入住'
+        },
+        {
+          bid:'2',
+          htype:'大床房',
+          pname:'王子杰',
+          inDay:'2020-1-2',
+          outDay:'2021-2-3',
+          status: '已入住'
+        }
+      ],
+      total: 67,
+      currentPage: 1
     };
   },
-
-  components: {},
+  components: { mHeader, mDetail, Pagination },
 
   computed: {},
 
+  watch:{
+    // 页数发生变化时候改变数据  包括页数为1时
+    currentPage: {
+      handler(){
+        this.fetchData();
+      },
+      immediate: true
+    }
+  },
+
   mounted() {},
 
-  methods: {}
+  methods: {
+    handleCurrentChange(val) {
+      this.currentRow = val;
+    },
+    showDetail(row){
+      this.detailVisible = true;
+      this.detailRow = row;
+    },
+    del(row){
+      this.$confirm('确认删除？')
+        .then(_ => {
+          
+        })
+        .catch(_ => {});
+    },
+    async fetchData(){
+      try{
+        // 获取数据
+      }catch(error){
+        console.log('all-order', error);
+      }finally{
+
+      }
+    }
+  }
 }
 
 </script>
---
+++ src/views/all-order/module/detail.vue
@@ -0,0 +1,104 @@
+<!--  -->
+<template>
+  <div class="detail">
+    <el-drawer
+      title="详情"
+      :visible.sync="drawerVisible"
+      size="30%"
+      :before-close="handleClose"
+      >
+      <el-form
+        ref="form"
+        :model="form"
+        label-width="80">
+          <el-form-item label="订单号">
+            {{ form.bid }}
+          </el-form-item> 
+          <el-form-item label="用户名">
+            {{ form.pname }}
+          </el-form-item>
+          <el-form-item label="手机号">
+            {{ form.phone }}
+          </el-form-item>
+          <el-form-item label="身份证">
+            {{ form.pIdentity }}
+          </el-form-item>
+          <el-form-item label="房间类型">
+            {{ form.htype }}
+          </el-form-item>
+          <el-form-item label="状态">
+            <template>
+              <el-tag type="danger">{{ form.status }}</el-tag>
+            </template>
+          </el-form-item>
+          <el-form-item label="入住日期">
+            {{ form.inDay + '-' + form.outDay}}
+          </el-form-item>
+          <el-form-item label="天数">
+            {{ form.days + ' 天'}}
+          </el-form-item>
+          <el-form-item label="总价">
+            {{ form.money + ' 元' }}
+          </el-form-item>
+      </el-form>
+    </el-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: undefined
+    },
+    detailRow: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data () {
+    return {
+      form: {
+        bid: 222,
+        inDay: '2020-3-8',
+        outDay: '2031-3-2',
+        days: '4',
+        status: '已入住',
+        pname: '姚思佳',
+        phone: '1222222',
+        pIdentity: '223444',
+        money: '34',
+        htype: '豪华大床房'
+      }
+    };
+  },
+
+  components: {},
+
+  computed: {
+    drawerVisible() {
+      return this.visible;
+    }
+  },
+
+  mounted() {},
+
+  methods: {
+    handleClose() {
+      this.$emit('update:visible', false);
+    }
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+.el-form{
+  padding: 20px;
+  .el-form-item{
+    .el-input{
+      width: 60%;
+    }
+  }
+}
+</style>
