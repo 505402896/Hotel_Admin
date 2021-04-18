1298b51a011c0e856bda4fb94f04a0847eebac32
505402896 <505402896@qq.com>
2021-04-18 23:03:29 +0800

    U 用户管理

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -1,24 +1,61 @@
 <!--  -->
 <template>
-  <div>
+  <div class="container">
+    <m-header />
+    <el-table
+      ref="singleTable"
+      :data="tableData"
+      highlight-current-row
+      @current-change="handleCurrentChange"
+      style="width: 100%;margin:0 auto;">
+      <el-table-column type="index" label="序号" />
+      <el-table-column prop="username" label="账号" />
+      <el-table-column prop="realName" label="真实姓名" />
+      <el-table-column prop="idCard" label="身份证" />
+      <el-table-column prop="gender" label="性别">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.gender" type="danger" effect="plain">女</el-tag>
+          <el-tag v-else type="info" effect="plain">男</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="phone" label="手机号" />
+    </el-table>
+    <pagination :total="total" :current-page.sync="currentPage" />
   </div>
 </template>
 
 <script>
+import mHeader from './module/header'
+import Pagination from '@/components/common/Pagination'
 export default {
   name:'user-manage',
   data () {
     return {
+      tableData: [
+        {
+          username: 'www',
+          realName: '王子杰',
+          idCard: '2222',
+          gender: 1,
+          phone: '15858300920'
+        }
+      ],
+      total: 67,
+      currentPage: 1
     };
   },
 
-  components: {},
+  components: { mHeader, Pagination },
 
   computed: {},
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    handleCurrentChange(val) {
+      this.currentRow = val;
+    },
+  }
 }
 
 </script>
---
+++ src/views/user-manage/module/header.vue
@@ -0,0 +1,42 @@
+<!--  -->
+<template>
+  <div class="header">
+    <el-row>
+      <el-col :span="4">
+        <el-input placeholder="根据用户名搜索" size="small" clearable v-model="username"/>
+      </el-col>
+      <el-col :span="2">
+        <el-button type="success" icon="el-icon-search" size="small">搜索</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bid: undefined,
+      username: undefined
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style lang="scss" scoped>
+.el-row{
+  margin: 20px auto;
+  margin-top: 80px;
+  .el-col{
+    margin-right: 20px;
+  }
+}
+</style>
