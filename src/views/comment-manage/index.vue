49ce836a4e52c6d61aad0fa3beec99c0fed9df5c
505402896 <505402896@qq.com>
2021-04-18 23:04:25 +0800

    U 评论管理

--- src/views/comment-manage/index.vue
+++ src/views/comment-manage/index.vue
@@ -1,24 +1,100 @@
 <!--  -->
 <template>
-  <div>
+  <div class="container">
+    <m-header />
+    <el-table
+      ref="singleTable"
+      :data="tableData"
+      highlight-current-row
+      @current-change="handleCurrentChange"
+      style="width: 100%;margin:0 auto;">
+      <el-table-column type="index" label="序号"/>
+      <el-table-column prop="bid" label="订单号"/>
+      <el-table-column prop="username" label="用户名"/>
+      <el-table-column prop="content" label="评论内容" />
+      <el-table-column prop="star" label="评分">
+        <template slot-scope="scope">
+          <el-rate
+            v-model="scope.row.star"
+            disabled
+            show-score
+            text-color="#ff9900"
+            score-template="{value}">
+          </el-rate>
+        </template>
+      </el-table-column>
+      <el-table-column prop="response" label="酒店回复">
+        <template slot-scope="scope">
+          <template v-if="scope.row.isEdit">
+            <el-input size="small" v-model.trim="scope.row.response" clearable />
+          </template>
+          <template v-else>
+            <span>{{ scope.row.response }}</span>
+          </template>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <template v-if="scope.row.isEdit">
+            <el-button type="info" icon="el-icon-close" size="mini" @click="cancel(scope.row)">取消</el-button>
+            <el-button type="success" icon="el-icon-check" size="mini" @click="save(scope.row)">保存</el-button>
+          </template>
+          <template v-else>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="edit(scope.row)">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="del(scope.row)">删除</el-button>
+          </template>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination :total="total" :current-page.sync="currentPage"/>
   </div>
 </template>
 
 <script>
+import mHeader from './module/header'
+import Pagination from '@/components/common/Pagination'
 export default {
   name:'comment-manage',
   data () {
     return {
+      tableData: [
+        {
+          bid: '222',
+          username: 'wzj',
+          content: 'henhao',
+          star: 3.5,
+          response: '-',
+          isEdit: false
+        }
+      ],
+      total: 6,
+      currentPage: 1
     };
   },
 
-  components: {},
+  components: { mHeader, Pagination },
 
   computed: {},
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    handleCurrentChange(val) {
+      this.currentRow = val;
+    },
+    save(row){
+
+    },
+    cancel(row){
+      row.isEdit = false;
+    },
+    edit(row){
+      row.isEdit = true;
+    },
+    del(row){
+
+    }
+  }
 }
 
 </script>
---
+++ src/views/comment-manage/module/header.vue
@@ -0,0 +1,42 @@
+<!--  -->
+<template>
+    <div class="header">
+      <el-row>
+        <el-col :span="24">
+          <el-radio-group v-model="selectItem" size="small">
+            <el-radio-button label="all">全部</el-radio-button>
+            <el-radio-button label="replied">已回复</el-radio-button>
+            <el-radio-button label="unreply">未回复</el-radio-button>
+          </el-radio-group>
+        </el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selectItem: 'all'
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style lang="scss" scoped>
+.el-row{
+  margin: 20px auto;
+  margin-top: 80px;
+  .el-col{
+    margin-right: 20px;
+  }
+}
+</style>
